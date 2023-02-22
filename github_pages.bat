5ecd15a9543365eefd98ee3bc8790ffeb1eea958
Eyepan <iamteoggy@gmail.com>
2023-02-22 19:34:32 +0530

    github_pages

--- github_pages.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-
-echo "Running npm build to build to ./dist"
-npm run build
-echo "github actions"
-git add .
-git commit -m 'dist update'
-git subtree push --prefix dist origin gh-pages
